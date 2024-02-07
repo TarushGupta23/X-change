c0edcb2a2dd79baa20023f240a49307e03e74da9
TarushGupta23 <tarush23.gupta@gmail.com>
2024-02-07 19:39:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/x_change/ChatActivity.kt
@@ -0,0 +1,11 @@
+package com.example.x_change
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
