0811e58da5c7aea6097ffd56c5af5be708f49f6a
TarushGupta23 <tarush23.gupta@gmail.com>
2024-02-08 17:59:02 +0530

    update profile ui

---
+++ app/src/main/java/com/example/x_change/ItemDisplayActivity.kt
@@ -0,0 +1,11 @@
+package com.example.x_change
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemDisplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_display)
+    }
+}
