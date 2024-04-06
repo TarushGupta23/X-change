5e9efa5e4a06569c01ca199d55bae0f799449e37
TarushGupta23 <tarush23.gupta@gmail.com>
2024-04-06 22:02:56 +0530

    finish chat activity

--- app/src/main/java/com/example/x_change/ChatActivity.java
+++ app/src/main/java/com/example/x_change/ChatActivity.java
@@ -3,6 +3,7 @@ package com.example.x_change;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -19,6 +20,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -34,6 +38,7 @@ public class ChatActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     EditText msgField;
     ChatActivityAdapter adapter;
+    private final StorageReference storageRef = FirebaseStorage.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +48,15 @@ public class ChatActivity extends AppCompatActivity {
         chatId = getIntent().getStringExtra("chatId");
         reference = reference.child(chatId);
 
-        profileImg = findViewById(R.id.chatActivity_profileImage); //TODO - click profile to reach profile img
+        profileImg = findViewById(R.id.chatActivity_profileImage);
         sendBtn = findViewById(R.id.chatActivity_sendBtn);
         backBtn = findViewById(R.id.chatActivity_backBtn);
         recyclerView = findViewById(R.id.chatActivity_recyclerView);
         msgField = findViewById(R.id.chatActivity_txtField);
 
-        adapter = new ChatActivityAdapter();
-//        adapter;
+        adapter = new ChatActivityAdapter(chatList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         backBtn.setOnClickListener(view -> {
             finish();
@@ -82,11 +88,19 @@ public class ChatActivity extends AppCompatActivity {
                     Chat c = snap.getValue(Chat.class);
                     chatList.add(c);
                 }
-
+                adapter.notifyDataSetChanged();
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
+        loadImageFromFirebase();
+    }
+
+    public void loadImageFromFirebase() {
+        storageRef.child(chatId.replace(uId, "")).child("userProfileImage").getDownloadUrl().addOnSuccessListener(uri -> {
+            Picasso.get().load(uri).into(profileImg);
         });
     }
 }
--- app/src/main/java/com/example/x_change/adapters/ChatActivityAdapter.java
+++ app/src/main/java/com/example/x_change/adapters/ChatActivityAdapter.java
@@ -1,6 +1,70 @@
 package com.example.x_change.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ChatActivityAdapter extends RecyclerView.Adapter {
+import com.example.x_change.R;
+import com.example.x_change.utility.Chat;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+public class ChatActivityAdapter extends RecyclerView.Adapter<ChatActivityAdapter.ViewHolder> {
+    ArrayList<Chat> list;
+    final String uId = FirebaseAuth.getInstance().getUid();
+
+    public ChatActivityAdapter(ArrayList<Chat> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v;
+        if (viewType == R.layout.chat_sent) {
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_sent, parent, false);
+        } else {
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_received, parent, false);
+        }
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.onBind(list.get(position));
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Chat chat = list.get(position);
+        if (chat.sentBy.equals(uId)) {
+            return R.layout.chat_sent;
+        } else {
+            return R.layout.chat_received;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView msg, date;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            msg = itemView.findViewById(R.id.tv1);
+            date = itemView.findViewById(R.id.tv2);
+        }
+
+        public void onBind(Chat chat) {
+            msg.setText(chat.msg);
+            date.setText(chat.date);
+        }
+    }
 }
