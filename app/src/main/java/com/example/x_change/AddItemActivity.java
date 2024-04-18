0ded6910de9b4e37846694943de350f587b8e1b5
TarushGupta23 <tarush23.gupta@gmail.com>
2024-04-18 14:33:30 +0530

    update add item activity

--- app/src/main/java/com/example/x_change/AddItemActivity.java
+++ app/src/main/java/com/example/x_change/AddItemActivity.java
@@ -1,15 +1,118 @@
 package com.example.x_change;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.x_change.utility.SwappingItem;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class AddItemActivity extends AppCompatActivity {
+    boolean mainImgAdded = false;
+    Uri mainImgUri;
+    ArrayList<Uri> imageUris = new ArrayList<>();
+    Button addBtn, cancelBtn;
+    CardView addImageBtn;
+    ImageView mainImg;
+    RecyclerView recyclerView;
+    EditText name, value, description, lookingFor;
+    final int MAIN_IMG_REQ = 5, IMG_REQ = 6;
+    ArrayList<String> imgList = new ArrayList<>();
+    DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("items");
+    StorageReference storageRef = FirebaseStorage.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
-        // TODO this
+
+        recyclerView = findViewById(R.id.addItem_RV);
+        mainImg = findViewById(R.id.addItem_mainImg);
+        addBtn = findViewById(R.id.addItem_add);
+        cancelBtn = findViewById(R.id.addItem_cancel);
+        addImageBtn = findViewById(R.id.addItem_addImg);
+        name = findViewById(R.id.addItem_name);
+        value = findViewById(R.id.addItem_value);
+        description = findViewById(R.id.addItem_description);
+        lookingFor = findViewById(R.id.addItem_lookingFor);
+
+        cancelBtn.setOnClickListener(view -> {
+            finish();
+        });
+
+        addBtn.setOnClickListener(view -> {
+            String itemName = name.getText().toString();
+            String itemValue = value.getText().toString();
+            String itemDescription = description.getText().toString();
+            String itemLookingFor = lookingFor.getText().toString();
+
+            if (itemName.isEmpty() || itemValue.isEmpty() || itemDescription.isEmpty() || itemLookingFor.isEmpty() || !mainImgAdded) {
+                Toast.makeText(this, "Please enter data", Toast.LENGTH_SHORT).show();
+            } else {
+                SwappingItem item = new SwappingItem(itemName, "", FirebaseAuth.getInstance().getUid(), itemDescription, itemLookingFor, itemValue, imgList, "mainImg");
+                addItemToDatabase(item);
+            }
+        });
+
+        addImageBtn.setOnClickListener(view -> {
+            if (!mainImgAdded) {
+                fileSelector(MAIN_IMG_REQ);
+            } else {
+                fileSelector(IMG_REQ);
+            }
+        });
+    }
+
+    void addItemToDatabase(SwappingItem item) {
+        DatabaseReference newSlot = reference.push();
+        item.itemId = newSlot.getKey();
+        storageRef = storageRef.child(item.itemId);
+        newSlot.setValue(item);
+        uploadPic("mainImg", mainImgUri);
+        for (int i=0; i<imageUris.size(); i++) {
+            uploadPic(i+"", imageUris.get(i));
+        }
+    }
+
+    public void fileSelector(int reqCode) {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(i, reqCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==MAIN_IMG_REQ && resultCode == RESULT_OK) {
+            mainImgUri = data.getData();
+            Picasso.get().load(mainImgUri).into(mainImg);
+            mainImgAdded = true;
+        } else {
+            imageUris.add(data.getData());
+            imgList.add((imgList.size() - 1) + "");
+            // TODO : create adapter and add item uri
+        }
+    }
+
+    public void uploadPic(String position, Uri uri) {
+        storageRef.child(position).putFile(uri);
     }
 }
